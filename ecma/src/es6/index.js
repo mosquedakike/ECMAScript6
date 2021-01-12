7f9bfc6cbeb2948c458d4508ba71b6130a4a0c1a
mosquedakike <enrique_inf@hotmail.com>
2021-01-12 17:59:11 -0600

    destructuracion

--- ecma/src/es6/index.js
+++ ecma/src/es6/index.js
@@ -11,15 +11,37 @@ function newFunctionTwo(name = 'Luis', age = 28, country = 'Morelos') {
 }
 
 newFunction();
-newFunctionTwo('juan', 49, 'bogota');
+newFunctionTwo('Pablo', 49, 'bogota');
 
-// Templates
+//versiones previas a ES6
 let hello = 'Hello'
 let world = 'World'
 let comment = hello + ' ' + world
 console.log(comment);
 
-//templates literals
+//es6
 let commentTwo = `${hello} ${world}`
 console.log(commentTwo);
 
+//multilineas en versiones previas
+let lorem = 'Esto es una frase \n' + 'Otra parrafo concatenado'
+
+//em6
+let loremTwo = `segunda parrafo
+esto ya es otra linea en el codigo`
+
+console.log(lorem);
+console.log(loremTwo);
+
+
+//destructuracion de elementos
+let person = {
+    'name': 'enrique',
+    'age': 26,
+    'country': 'MX' 
+}
+
+console.log(person.name, person.age, person.country);
+
+let {name, age, country} = person2;
+console.log(name, age, country);
