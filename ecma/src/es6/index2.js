59ae0320b63de23502d2f9fff5a79916cfeaf32e
Enrique Hernandez <enrique_inf@hotmail.com>
2021-01-27 23:29:17 -0600

    generadores

--- ecma/src/es6/index2.js
+++ ecma/src/es6/index2.js
@@ -71,3 +71,16 @@ console.log(calc.sum(5,10));
 import { hello }  from './module';
 
 hello();
+
+function* helloWorld() {
+    if (true) {
+        yield 'Hello,'
+    }if (true) {
+        yield 'World'
+    }
+}
+
+const generatorHello = helloWorld()
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
