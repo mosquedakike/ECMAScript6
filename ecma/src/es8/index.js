f0064cbb71a4914088e1339a7f2d744982b36be9
Enrique Hernandez <enrique_inf@hotmail.com>
2021-01-28 23:54:57 -0600

    entries & values

---
+++ ecma/src/es8/index.js
@@ -0,0 +1,16 @@
+//es8
+const data = {
+    frontend: 'Enrique',
+    backend: 'Luis',
+    design: 'Ana',
+}
+
+//contenido del objeto
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+//valores
+const values = Object.values(data)
+console.log(values);
+console.log(values.length);
