2600b6a583fed532f2e81a55e5734c88a04d857c
Enrique Hernandez <enrique_inf@hotmail.com>
2021-02-03 10:22:26 -0600

    resolviendo conflictos con es7

--- ecma/src/es7/index.js
+++ ecma/src/es7/index.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-let numbers = [1,2,3,7,8];
-
-if (numbers.includes(7)) {
-    console.log('Si existe el numero 7 en el arreglo');
-}else{
-    console.log('Este valor no se encuentra en el arreglo');
-}
-=======
 let numbers = [1,2,3,4,7,8]
 
 if (numbers.includes(7)) {
@@ -21,4 +12,3 @@ let exponent = 3;
 let result = base ** exponent
 
 console.log(result);
->>>>>>> a54c523b0feea6745164d61104e1dfec4cf822a1
