f5b8df464f314638f63c3d52a59c2a20bb4fe375
Enrique Hernandez <enrique_inf@hotmail.com>
2021-02-03 10:05:07 -0600

    agragando carpeta es7

---
+++ ecma/src/es7/index.js
@@ -0,0 +1,7 @@
+let numbers = [1,2,3,7,8];
+
+if (numbers.includes(7)) {
+    console.log('Si existe el numero 7 en el arreglo');
+}else{
+    console.log('Este valor no se encuentra en el arreglo');
+}
